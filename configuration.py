d8d083651c3c8e06c7934c14dacf9f912648aeb3
Pertussis <akimenkoaya@gmail.com>
2023-12-03 14:10:28 +0000

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://aa6d9ab1-8799-4cab-b810-e5cbc44c8244.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
